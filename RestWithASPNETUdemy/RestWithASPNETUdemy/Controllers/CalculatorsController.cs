9930f4a8125efdcdcb10b77f9b02ce826dc04ed6
Tiago Rodrigues <tiago.r.p@hotmail.com>
2018-12-23 18:20:56 -0200

    Versão Atualizada da calculadora

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/CalculatorsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorsController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                decimal sum = decimal.Parse(firstNumber) + decimal.Parse(secondNumber);
+                return Ok(sum);
+            }
+
+            return BadRequest("Invalid input");
+        }
+
+        private bool IsNumeric(string number)
+        {
+            double _number = 0;
+
+            return double.TryParse(number, out _number);
+        }
+    }
+}
